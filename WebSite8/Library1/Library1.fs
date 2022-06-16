3ca48388d5f42981251dba42b2187929c224fdc8
mickyacj <105824904+mickyacj@users.noreply.github.com>
2022-06-16 00:07:21 -0400

    fibonacciWeb

---
+++ WebSite8/Library1/Library1.fs
@@ -0,0 +1,27 @@
+﻿namespace Library1
+
+open System
+module fibo=
+    Console.WriteLine("Ingrese número de términos: ")
+    let n=Convert.ToInt32(Console.ReadLine())
+
+    let mutable a=0
+    let mutable b=1
+
+    for i=1 to n do
+        if i=1 then
+            printfn "%d" a
+        elif i=2 then
+            printfn "%d" b
+        else
+            
+            let c=a+b
+            printfn "%d" c
+            a<-b
+            b<-c
+    
+    let tecla=Console.ReadKey()
+    printfn("");
+
+type Class1() = 
+    member this.X = "F#"
